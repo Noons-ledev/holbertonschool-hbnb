a33edfebf7ce1567218276ec36bc4fbd802c23e0
Mollydayne <cl.w.perez@gmail.com>
2025-03-10 15:16:42 +0100

    Task 6 : part 1, base model mapping

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -5,14 +5,14 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
-from .config import config
+from config import DevelopmentConfig
 
 db = SQLAlchemy()
 
-def create_app(config_class="development"):
+def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
 
-    app.config.from_object(config[config_class])
+    app.config.from_object(config_class)
 
     db.init_app(app)
 
--- part3/app/models/basemodel.py
+++ part3/app/models/basemodel.py
@@ -1,28 +1,38 @@
+from app import db
 import uuid
 from datetime import datetime
+from sqlalchemy.sql import func
+
 
 class BaseModel:
     def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        __abstract__ = True
+
+        id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+        created_at = db.Column(db.DateTime, default=func.now())
+        updated_at = db.Column(db.DateTime, default=func.now(), onupdate=func.now())
+
+        #Molly : utcnow isnt used naymore. We could replace it by func.now, sqlalchemy will handle everything
 
     def save(self):
-        """Update the updated_at timestamp whenever the object is modified"""
-        self.updated_at = datetime.now()
+        self.updated_at = func.now()
+        db.session.add(self)
+        db.session.commit()
 
     def update(self, data):
-        """Update the attributes of the object based on the provided dictionary"""
         for key, value in data.items():
             if hasattr(self, key):
                 setattr(self, key, value)
-        self.save()  # Update the updated_at timestamp
-        
-    def is_max_length(self, name, value, max_length):
+        self.save()
+
+#Molly : changing these two for a static method, because they dont need self
+
+    @staticmethod
+    def is_max_length(name, value, max_length):
         if len(value) > max_length:
-            raise ValueError(f"{name} must be {max_length} characters max.") 
-        
-    
-    def is_between(self, name, value, min, max):
+            raise ValueError(f"{name} must be {max_length} characters max.")
+
+    @staticmethod
+    def is_between(name, value, min, max):
         if not min < value < max:
             raise ValueError(f"{name} must be between {min} and {max}.")
